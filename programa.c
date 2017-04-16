baa65ec5f5ceebbaa3a63ecc3ccb96fe4e878a69
ipina <ipina@qi.fcen.uba.ar>
2017-04-16 20:49:32 -0300

    este commit es de paranoico, percola2 es mucho mas rapido pero todavia no lo termine

--- programa.c
+++ programa.c
@@ -10,9 +10,11 @@
 
 void  llenar(int *red,int n,float prob); //llena con 1 y ceros con prob p de ser 1. (falta programar)
 
-void mostrar(int *red, int n); // muestra la matriz en la pantalla
+void mostrar(int *red, int n1, int n2); // muestra la matriz en la pantalla
 
-void mostramostro(int *red, int *clase, int n, char *k);//Puedo corregir la forma que me muestra los pasos en todo el codigo.
+void mostra1(int *red, int n1, int n2, char *k); //muestra solo una matriz con un mensaje adicional
+
+void mostramostro(int *red, int *clase, int n1, int n2, char *k);//Puedo corregir la forma que me muestra los pasos en todo el codigo.
 
 int   hoshen(int *red,int n);			// hace el algoritmo de reemplazar todo por etiquetas de cluster y las repara
 
@@ -24,6 +26,8 @@ int  etiqueta_falsa(int *red,int *clase,int s1,int s2, int i, int n); //Voy a us
 //void  corregir_etiqueta(int *red,int *clase,int n); // no la voy a usar
 
 int   percola(int *red,int n, int frag); //esta si! revisa si el cluster percola o no.
+
+int   percola2(int *red,int n, int frag); //esta si! revisa si el cluster percola o no.
 
 int main(int argc,char *argv[])
 {
@@ -57,9 +61,11 @@ int main(int argc,char *argv[])
          frag=hoshen(red,n);
         
          denominador=2.0*denominador;
-	 if (percola(red,n, frag)) 
+	 printf("Inicia chequeo Percolacion\n");
+	 percola2(red,n,frag);
+	 if (percola2(red,n,frag)) 
          	{
-			printf("PERCOLAAAAA");
+			printf("PERCOLAAAAA\n");
         		prob+=(-1.0/denominador); 
 		}
         else prob+=(1.0/denominador);
@@ -81,11 +87,11 @@ int hoshen(int *red,int n)
     a cada fragmento de red. 
   */
 
-  int i,j,k,s1,s2,frag,l,s; //inicializa todo
+  int i,j,k,s1,s2,frag,s; //inicializa todo
   int *clase;		//empieza una etiqueta clase que guarda las etiquetas que va usando para el cluster que está distinguiendo
 
   frag=0;	//inicializa la clase temp en 0, esta es la izq del primer elemento
-  l=0;
+  
   
   clase=(int *)malloc(n*n*sizeof(int));	//reserva el espacio para clase
 
@@ -95,20 +101,20 @@ int hoshen(int *red,int n)
 
   s1=0;	//definimos que el valor a izq de la posicion va a ser cero (no existe, no hay periodicidad)
   frag=2;//primera etiqueta de la clase
-if (*red) *clase=frag;
+if (*red) *red=frag;
 
-	 // mete la posición red, es decir la posicion primera, NO su valor y asi sé qué posición estoy asignando, asi como la clase, el valor de la posicion en la izquierda y frag, la etiqueta que voy actualizando
-mostramostro(red, clase, n, "Primer elemento");  
+	 // Si el primer valor de red esta ocupado, entonces asigno la clase automaticamente
+mostra1(red, n,n, "Primer elemento");  
   // primera fila de la red
 
   for(i=1;i<n;i++) 
     {
       if (*(red+i)) 
          {
-           s1=*(clase+i-1);  //etiqueta que vamos a poner despues a este cluster
-           frag=actualizar(red+i,clase+i,s1,frag);//ponerle el numero de frag(o del que tiene atras) y asignarle esa etiqueta a la posicion de clase si el anterior es 0 subir la clase usada, es decir subir el return en y ponerle esa clase nueva a la posicion de clase
+           s1=*(red+i-1);  //etiqueta que vamos a poner despues a este cluster
+           frag=actualizar(red+i,clase+i,s1,frag);//ponerle el numero de frag(o del que tiene atras) y asignarle esa etiqueta a la posicion de red, SI el anterior es 0 subir la clase usada, es decir subir el return frag en 1 y ponerle esa clase nueva a la posicion de clase
 
-		mostramostro(red, clase, n, "Primera fila"); 
+		mostra1(red, n,n, "Primera fila"); 
          }
     }
 
@@ -122,29 +128,29 @@ mostramostro(red, clase, n, "Primer elemento");
 
       if (*(red+i)) 
          {
-           s1=*(clase+i-n); 
+           s1=*(red+i-n); 
            frag=actualizar(red+i,clase+i,s1,frag);
 	
-		mostramostro(red,clase,n, "Cuerpo ppal");
+		mostra1(red,n,n, "Cuerpo ppal");
          }
 	//aca comienza con el codigo en todas las filas
 
       for(j=1;j<n;j++)
 	if (*(red+i+j))
 	  {
-	    s1=*(clase+i+j-1); 
-            s2=*(clase+i+j-n);
+	    s1=*(red+i+j-1); 
+            s2=*(red+i+j-n);
 
 	    if (s1*s2>0)
 	      {
 		s=etiqueta_falsa(red,clase,s1,s2,i,n);
-		*(clase+i+j)=s;
+		*(red+i+j)=s;
 	      }
 	    else 
 	      { if (s1!=0) frag=actualizar(red+i+j,clase+i+j,s1,frag);
 	        else       frag=actualizar(red+i+j,clase+i+j,s2,frag);
 		
-		mostramostro(red, clase, n, "Cuerpo ppal");
+		mostra1(red, n,n, "Cuerpo ppal");
 	      }
 	  }
     }
@@ -162,13 +168,13 @@ int actualizar (int *logo,int *reclase,int s, int frag)
 	//No toma siempre una etiqueta nueva, si puede, hereda la etiqueta mas cercana.
 		if (s!=0)
 		{
-			*reclase=s;
+			*logo=s;
 		}  
 		else
 		{
 			frag++;	
 			
-			*reclase=frag;
+			*logo=frag;
 		}
 	return frag;
 	}
@@ -195,32 +201,47 @@ void  llenar(int *red,int n,float prob)//toma la red que le ponen, el lado y la 
 	}
 }
 
-void mostrar(int *print, int n)
+void mostrar(int *print, int n1, int n2)
 {
 	//basicamente recorre la variable print para imprimirla en forma de matriz
 	int  i,j;
 	
-	for (i=0;i<n;i++){
+	for (i=0;i<n1;i++){
 		printf("\n");
-		for (j=0;j<n;j++){
-			printf("%3i",*(print+i*n+j));
+		for (j=0;j<n2;j++){
+			printf("%3i",*(print+i*n1+j));
 	
 		}
 	}
 }
 
-void mostramostro(int *red, int *clase, int n, char *k)
+
+void mostra1(int *red, int n1, int n2, char *k)
 {
 	//imprime de la forma que yo quiera los resultados
 	printf("%s\n", k); 
-	//mostrar(red,n);
+	mostrar(red,n1, n2);
 	printf("\n");
-	mostrar(clase,n);
+	
+	
+	 
+	getchar();  
+}
+
+void mostramostro(int *red, int *clase, int n1, int n2, char *k)
+{
+	//imprime de la forma que yo quiera los resultados
+	printf("%s\n", k); 
+	mostrar(red,n1, n2);
+	printf("\n");
+	mostrar(clase,n1, n2);
 	printf("\n");
 	
 	 
 	getchar();  
 }
+
+
 
 int etiqueta_falsa(int *red,int *clase,int s1,int s2,int i, int n)
 {	
@@ -244,16 +265,16 @@ int etiqueta_falsa(int *red,int *clase,int s1,int s2,int i, int n)
 		printf("sp=%i\n",sp);
 		for(k=0;k<(i/n)+1;k++){
 			for(l=0;l<n;l++){
-				printf("valor anterior=%i\n",*(clase+k*n+l));
+				printf("valor anterior=%i\n",*(red+k*n+l));
 				printf("fila analizada=%i\n",k+1);
 				printf("col analizada=%i\n",l+1);
-				if ((*(clase+k*n+l))==sp){ 
+				if ((*(red+k*n+l))==sp){ 
 				
-					*(clase+k*n+l)=s;
+					*(red+k*n+l)=s;
 				
 					
 				}
-				mostramostro(red, clase,n, "Unión de clusters");
+				mostra1(red,n,n, "Unión de clusters");
 			}
 		}
 	}	
@@ -274,36 +295,42 @@ int etiqueta_falsa(int *red,int *clase,int s1,int s2,int i, int n)
 //	}
 //}
 
-int percola (int *clase,int n,int frag)
+int percola (int *red,int n,int frag)
 {
 	int f,i, *primer, *segundo, t1, t2, k,a;
 	a=0;
-	f=-frag;
+	f=2;
 	k=n*n;
-	while (f<frag+1 && a!=0  )
+	primer=(int *)malloc(frag*sizeof(int));
+	segundo=(int *)malloc(frag*sizeof(int));
+	printf("etiqueta maxima : %i \n",frag);
+	while (f<frag+1 && a==0 )
 	{
-		*(primer+f-1)=0;
-		*(segundo+f-1)=0;
+		*(primer+f-2)=0;
+		*(segundo+f-2)=0;
 		t1=0;
 		t2=0;
 		i=0;
-		while(i<(n-1) && (t1==0||t2==0))
+		while(i<n && (t1==0 || t2==0))
 		{
 			
-				if (*(clase+i)==f) 
+				if (*(red+i)==f) 
 				{
-					*(primer+f)=1;
+					*(primer+f-2)=1;
 					t1=1;
 				
 				}
-				if (*(clase+k-n+i)=f) 
+				if (*(red+k-n+i)==f) 
 				{
-					*(segundo+f)=1;
+					*(segundo+f-2)=1;
 					t2=1;
 				
 				}
-			
-			i++;
+			mostra1(red,n,n, "¿Percola esta red?");
+			mostramostro(primer, segundo,1,n, "Parcialmente primera y ultima fila");
+			printf("Chequeando etiqueta %i\n",f);
+			printf("Chequeando columna %i\n",i+1);
+			i=i+1;
 		
 		}
 		if ((*(primer+f))&&(*(segundo+f))) 
@@ -312,12 +339,53 @@ int percola (int *clase,int n,int frag)
 			
 		}
 		else *(primer+f)=0;
-	f++;
+
+		f=f+1;
 	}
 	return a;
 	
 }
 			
-	
+int percola2	(int *red,int n,int frag)
+{
+	int i, *primer, *segundo,k,a;
+	primer=(int *)malloc(frag*sizeof(int));
+	segundo=(int *)malloc(frag*sizeof(int));
+	k=n*n-n;
+	printf("este es k: %i\n",k);
+	a=0;
+	for(i=0;i<frag;i++) 
+	{	
+		*(primer+i)=0;
+		*(segundo+i)=0;
+	}
+	for(i=0;i<n;i++)
+	{	
+		if (*(red+i))
+		{
+			*(primer+*(red+i)-1)=1;
+		
+			
+		}
+		if (*(red+k+i))
+		{
+			*(segundo+k+*(red+i)-1)=1;
+		}
+		mostra1(red,n,n, "¿Percola esta red?");
+		mostramostro(primer,segundo, 1,frag, "Parcialmente etiquetas ocupadas por primera y ultima fila\n");
+	}
+	i=0;
+	while(i<frag && a!=0) 
+	{
+		if ((*(primer+i))&&(*(segundo+i))) 
+			{
+				a=1;
+				
+			}
+		i=i+1;
+	}
+	return a;
+}
+		
 	 
 	
